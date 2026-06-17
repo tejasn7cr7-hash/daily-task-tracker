9ec9f6416eda7343de1f37e347f88fe5ce2002aa
tejasn7cr7-hash <tejasn7cr7@gmail.com>
2026-06-17 21:22:15 +0530

    Delete habit_tracker directory

--- habit_tracker/start_reminders.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing reminder dependencies...
-pip install plyer requests
-echo.
-echo Starting Reminder Service...
-echo Keep this window open to receive notifications!
-echo.
-python reminder.py
-pause
